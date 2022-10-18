c6cbfa8412e2784e2721d9d38a5eb15a33d428d5
Matthew Gidcomb <matthew.gidcomb@outlook.com>
2022-10-17 20:33:14 -0400

    Add top-level 404 and 500 fallbacks

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const debug = require('debug')('gantri-challenge:app');
 
 const { sequelize } = require('./db/models');
 const artRouter = require('./routes/art');
@@ -18,5 +19,16 @@ app.set('sequelize', sequelize);
 
 app.use('/api/art', artRouter);
 app.use('/api/users', usersRouter);
+
+// 404 (explicitly defined to make the response JSON, for consistency across API)
+app.use(function (req, res) {
+  res.status(404).json({ message: 'not found' });
+});
+
+// general error-handler
+app.use(function (err, req, res, next) {
+  debug(err);
+  res.status(500).json({ message: 'an unknown error occurred' });
+});
 
 module.exports = app;
